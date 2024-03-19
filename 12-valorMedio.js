18ade747ca332c758eab8f1aa6481c7e4c63f5e5
SofiDevO <angiemeow89@gmail.com>
2024-03-18 23:23:45 -0600

    Clase 12

---
+++ 12-valorMedio.js
@@ -0,0 +1,12 @@
+const notas = [7, 5, 9, 8, 7.5, 4, 10, 9, 8];
+let sumaNotas = 0;
+
+for (let i = 0; i < notas.length; i++) {
+ sumaNotas += notas[i];
+};
+
+
+const mediaAritmetica = sumaNotas/notas.length;
+console.log(sumaNotas);
+
+console.log(`El valor de la media Aritmética es de ${mediaAritmetica.toFixed(2)}`)
