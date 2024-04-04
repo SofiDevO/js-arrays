31c11f949d6178eef6a6ff3fbfeec0068a290111
SofiDevO <angiemeow89@gmail.com>
2024-04-04 02:19:36 -0600

    Curso terminado

--- 21-repetidos.js
+++ 21-repetidos.js
@@ -1,1 +1,21 @@
 const lstAlumnos = ['Jose', 'Maria','Jose', 'Luis', 'Jose', 'Maria'];
+const alumnosUnicos = [...new Set(lstAlumnos)];
+
+
+console.log(alumnosUnicos);
+
+
+
+const numeros = [43, 50, 65, 12]
+
+const suma = numeros.reduce((acc, actual) => actual + acc, 0)
+
+console.log(suma) //170
+
+
+//
+function suma(a,b,c,d){
+    return a + b + c + d;
+}
+
+console.log(suma(43, 50, 65, 12));
